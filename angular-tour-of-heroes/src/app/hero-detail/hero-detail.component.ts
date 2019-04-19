c130e79ef52a1f944a955c8f4ab6ea89eca55a69
Mario R <mario.rodr.214@gmail.com>
2019-04-18 20:31:00 -0500

    Finished hero search

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -23,9 +23,21 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
     this.getHero();
   }
+
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id'); //route.snapshot is a static image of the route information shortly after the component was created.
+    //The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+    //Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is what a hero id should be.
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
