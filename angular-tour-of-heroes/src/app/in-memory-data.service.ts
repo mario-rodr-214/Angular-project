861fc4095ee93ad731df47ac1addca831a87d0b6
Mario R <mario.rodr.214@gmail.com>
2019-04-24 22:06:59 -0500

    added images to heroes and UI changes

--- angular-tour-of-heroes/src/app/in-memory-data.service.ts
+++ angular-tour-of-heroes/src/app/in-memory-data.service.ts
@@ -1,35 +1,25 @@
-import { Injectable } from '@angular/core';
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Hero } from './hero';
-
+import { Injectable } from "@angular/core";
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Hero } from "./hero";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class InMemoryDataService implements InMemoryDbService{
-
+export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: "Mr. Nice", avatar: "assets/avatars/profile1.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 12, name: "Narco", avatar: "assets/avatars/profile2.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 13, name: "Bombasto" , avatar: "assets/avatars/profile3.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 14, name: "Celeritas" , avatar: "assets/avatars/profile4.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 15, name: "Magneta" , avatar: "assets/avatars/profile5.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 16, name: "RubberMan" , avatar: "assets/avatars/profile6.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 17, name: "Dynama" , avatar: "assets/avatars/profile7.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 18, name: "Dr IQ" , avatar: "assets/avatars/profile8.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 19, name: "Magma" , avatar: "assets/avatars/profile9.jpg", icon:"assets/avatars/avatar-all.png"},
+      { id: 20, name: "Tornado" , avatar: "assets/avatars/profile10.jpg", icon:"assets/avatars/avatar-all.png"}
     ];
-    return {heroes};
-  
-  
-  
-  
-  
-  
-  
-  
+    return { heroes };
   }
   // Overrides the genId method to ensure that a hero always has an id.
   // If the heroes array is empty,
@@ -37,6 +27,8 @@ export class InMemoryDataService implements InMemoryDbService{
   // if the heroes array is not empty, the method below returns the highest
   // hero id + 1.
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+    return heroes.length > 0
+      ? Math.max(...heroes.map(hero => hero.id)) + 1
+      : 11;
   }
 }
